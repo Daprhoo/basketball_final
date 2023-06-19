1f01e3910b672f2d5819ca5fee1b08f978ed5014
Daprhoo <119071733+Daprhoo@users.noreply.github.com>
2023-06-19 18:23:55 +0300

    after sound

--- Assets/Code/TimerController.cs
+++ Assets/Code/TimerController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TimerController : MonoBehaviour
 {
@@ -8,35 +9,48 @@ public class TimerController : MonoBehaviour
 
     public Text timerText; // UI'da süreyi göstermek için Text elemanı
 
-    private void Update()
-    {
-        currentTime += Time.deltaTime; // Geçen süreyi güncelle
+    private bool isTimerRunning; // Timer'ın çalışıp çalışmadığını kontrol etmek için bool değişkeni
 
-        // Zaman aşımını kontrol et
-        if (currentTime >= totalTime)
-        {
-            // Zaman dolduğunda yapılması gereken işlemleri burada gerçekleştirebilirsiniz.
-            // Örneğin, oyunu durdurabilir veya sonuçları değerlendirebilirsiniz.
-        }
-
-        UpdateTimerUI(); // Zamanlayıcıyı güncellemek için UI elemanını güncelle
-    }
-
-    public void StartTimer()
+    private void Start()
     {
         currentTime = 0f; // Geçen süreyi sıfırla
+        isTimerRunning = true; // Timer'ı başlat
+    }
+
+    private void Update()
+    {
+        if (isTimerRunning)
+        {
+            currentTime += Time.deltaTime; // Geçen süreyi güncelle
+
+            // Zaman aşımını kontrol et
+            if (currentTime >= totalTime)
+            {
+                // Zaman dolduğunda yapılması gereken işlemleri burada gerçekleştirebilirsiniz.
+                // Örneğin, oyunu durdurabilir veya sonuçları değerlendirebilirsiniz.
+                isTimerRunning = false; // Timer'ı durdur
+                SceneManager.LoadScene(0); // Ana menü sahnesine geçiş yap
+            }
+
+            UpdateTimerUI(); // Zamanlayıcıyı güncellemek için UI elemanını güncelle
+        }
     }
 
     private void UpdateTimerUI()
-{
-    float remainingTime = totalTime - currentTime; // Kalan süreyi hesapla
+    {
+        float remainingTime = totalTime - currentTime; // Kalan süreyi hesapla
 
-    int minutes = Mathf.FloorToInt(remainingTime / 60f); // Kalan süreden dakikaları al
-    int seconds = Mathf.FloorToInt(remainingTime % 60f); // Kalan süreden saniyeleri al
+        int minutes = Mathf.FloorToInt(remainingTime / 60f); // Kalan süreden dakikaları al
+        int seconds = Mathf.FloorToInt(remainingTime % 60f); // Kalan süreden saniyeleri al
 
-    // Kalan süreyi kullanıcı arayüzünde göstermek için güncelleme yap
-    timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds); // Dakika ve saniye formatını kullanarak süreyi Text elemanına gönder
+        // Kalan süreyi kullanıcı arayüzünde göstermek için güncelleme yap
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds); // Dakika ve saniye formatını kullanarak süreyi Text elemanına gönder
+
+        if (remainingTime <= 0f)
+        {
+            isTimerRunning = false; // Timer'ı durdur
+            timerText.text = "00:00"; // Süre dolunca ekranda 00:00 göster
+            SceneManager.LoadScene(0); // Ana menü sahnesine geçiş yap
+        }
+    }
 }
-
-}
-
